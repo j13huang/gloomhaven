a2c31efe089aaf0879463cae2fd5e5cfc650f470
John Huang <j13huang@gmail.com>
2018-01-29 01:40:45 -0800

    swap monster card back with card

--- src/components/Monsters/Deck.js
+++ src/components/Monsters/Deck.js
@@ -7,13 +7,13 @@ import "./Deck.css";
 
 export function Deck({name, card, active}) {
     return (<div className="Monsters--Deck--Container">
-        <div className="Monsters--Deck--CardBack">
+        {!card && <div className="Monsters--Deck--CardBack">
             <img src={cardBack} className="Monsters--Deck--CardBackImage" alt="card back" />
             <div className="Monsters--Deck--CardBackName">{name}</div>
             {!active && <div className={"Monsters--Deck--InactiveCover"}>
                 Inactive
             </div>}
-        </div>
+        </div>}
         {card && <Card name={name} card={card} />}
     </div>);
 }
