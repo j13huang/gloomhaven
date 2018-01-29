6eb7901419010869c7d407945a40d4da64c12f8d
John Huang <j13huang@gmail.com>
2018-01-29 04:25:30 -0800

    xp_white

--- src/components/Players/PlayerTracker.js
+++ src/components/Players/PlayerTracker.js
@@ -2,6 +2,7 @@ import React from "react";
 import {connect} from "react-redux";
 
 import xpIcon from "./xp.svg";
+//import xpIcon from "./xp_white.svg";
 import {BonusSelectors} from "../UnitTracking/BonusSelectors";
 import {StatusEffectTracker} from "../UnitTracking/StatusEffectTracker";
 import {HPTracker} from "../UnitTracking/HPTracker";
